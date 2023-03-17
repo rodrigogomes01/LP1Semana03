a20fc0347f12208452fcc8b363c60afbf6544f83
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-17 14:04:54 +0000

    Exercise 3 - Check if the player has valid perks

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -6,31 +6,46 @@ namespace PlayerPerks
         static void Main(string[] args)
         {
             Perks P1Perks = 0;
+            bool PerkNotValid = false;
 
             // Check every character on the first argument gave
             foreach (char c in args[0])
             {
-
                 // Add a perk depending on the user's input
                 if (c == 'w')
                 {
                     P1Perks ^= Perks.WaterBreathing;
                 }
-                if (c == 's')
+                else if (c == 's')
                 {
                     P1Perks ^= Perks.Stealth;
                 }
-                if (c == 'a')
+                else if (c == 'a')
                 {
                     P1Perks ^= Perks.AutoHealing;
                 }
-                if (c == 'd')
+                else if (c == 'd')
                 {
                     P1Perks ^= Perks.DoubleJump;
+                }
+
+                else
+                {
+                    PerkNotValid = true;
                 }
             }
 
             Console.WriteLine(P1Perks);
+
+            ///Check the player's perks
+            if (PerkNotValid)
+            {
+                Console.WriteLine("Perk Unknown");
+            }
+            else
+            {
+                Console.WriteLine(P1Perks);
+            }
         }
     }
 }
