cddc0665c9c5def1da9460407bb6c85d2dc463ce
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-17 13:40:20 +0000

    Exercise 2 - Sum the values in each column and row

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -10,6 +10,7 @@ namespace ArraySums
             Console.Write("Enter the horizontal dimension of the array");
             string HorizontalDimension = Console.ReadLine();
             int hor_val = char.Parse(HorizontalDimension);
+
             ///Ask for the vertical dimension for the array
             Console.Write("Enter the vertical dimension of the array");
             string verticalDimension = Console.ReadLine();
@@ -17,16 +18,40 @@ namespace ArraySums
 
             float[,] array = new float[HorizontalDimension, verticalDimension];
 
-            ///Ask for number in i and j
+            /// Ask for number in i and j
             for (int i = 0; i < hor_val; i++)
             {
                 for (int j = 0; j < ver_val; j++)
                 {
-                    Console.Write($"Ask for a number in this array [{i},{j}]: ");
+                    Console.Write($"Ask for a number in this array [{i},{j}] ");
                     array[i, j] = float.Parse(Console.ReadLine());
                 }
             }
 
+            ///Sum the values in each row 
+            Console.WriteLine("Sum row");
+            for (int i = 0; i < hor_val; i++)
+            {
+                float SumRow = 0;
+                for (int j = 0; j < ver_val; j++)
+                {
+                    SumRow += array[i, j];
+                }
+                Console.WriteLine($"Row {i + 1}: {SumRow}");
+            }
+
+            ///Sum the values in each column 
+            Console.WriteLine("Sum column");
+            for (int j = 0; j < ver_val; j++)
+            {
+                float SumColumn = 0;
+                for (int i = 0; i < hor_val; i++)
+                {
+                    SumColumn += array[i, j];
+                }
+                Console.WriteLine($"Column {j + 1}: {SumColumn}");
+
+            }
         }
     }
 }
