910acc4d650f6fc3a30cdfc4b514efb9a35b2275
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-17 13:29:19 +0000

    Exercise 1 - Print a string omitting the character

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -5,9 +5,21 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.Write("Give me a character");
+            Console.Write("Give me a string");
+            string AskString = Console.ReadLine();
+
+            Console.Write("Give me  a character");
             string AskCharacter = Console.ReadLine();
             char Character = char.Parse(AskCharacter);
+
+            ///Print the string omitting the character
+            foreach (char c in AskString) 
+            {
+                if (c != Character)
+                {
+                    Console.Write(c);
+                }
+            }
         }
     }
 }
