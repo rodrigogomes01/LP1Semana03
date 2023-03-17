38c125891e0d2c8373545c1e4a8179887ee7b7bd
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-17 13:33:46 +0000

    Ask for the horizontal and vertical dimensions of a bidimensional array of floats

---
+++ ArraySums/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Ask for the horizontal dimension for the array
+            Console.Write("Enter the horizontal dimension of the array");
+            string HorizontalDimension = Console.ReadLine();
+            int hor_val = char.Parse(HorizontalDimension);
+
+            ///Ask for the vertical dimension for the array
+            Console.Write("Enter the vertical dimension of the array");
+            string verticalDimension = Console.ReadLine();
+            int ver_val = char.Parse(verticalQuestion);
+        }
+    }
+}
