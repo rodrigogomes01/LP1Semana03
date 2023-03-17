ddbb23d7e8aea5b76589314e3de9aba28844d2d6
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-17 13:25:08 +0000

    Exercise 1 - Ask for a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a string");
+            string AskString = Console.ReadLine();
+        }
+    }
+}
